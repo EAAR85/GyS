3c976c73e4fa80342c8239c3137da97053057212
EAAR85 <44047823+EAAR85@users.noreply.github.com>
2022-02-03 12:18:20 -0500

    net core: demo rest api

---
+++ WebDemo/Services/TodoDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebDemo.Entity;
+
+namespace WebDemo.Services
+{
+    public class TodoDBContext : DbContext
+    {
+        public TodoDBContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; }
+    }
+
+}
